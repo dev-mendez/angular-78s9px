4dcbdb80a2850831f90c683cb923502c0043f3a7
freedomxam <an2m001@gmail.com>
2019-09-19 23:07:40 -0400

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { ProductAlertsComponent } from './product-alerts/product-alerts.componen
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { CartService } from './cart.service';
 import { CartComponent } from './cart/cart.component';
+import {HttpClientModule} from '@angular/common/http';
+import { ShippingComponent } from './shipping/shipping.component';
 
 @NgModule({
   imports: [
@@ -20,7 +22,8 @@ import { CartComponent } from './cart/cart.component';
       {path: 'products/: productId',
       component: ProductDetailsComponent},{
         path:'cart',component: CartComponent
-      }
+      },
+      {path:'shipping', component:ShippingComponent}
     ])
   ],
   declarations: [
@@ -29,7 +32,8 @@ import { CartComponent } from './cart/cart.component';
     ProductListComponent,
     ProductAlertsComponent,
     ProductDetailsComponent,
-    CartComponent
+    CartComponent,
+    ShippingComponent
   ],
   bootstrap: [ AppComponent ],
   providers: [CartService]
